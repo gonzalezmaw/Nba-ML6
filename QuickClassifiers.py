c9e0e10eaf605e19f622fc053446f7cd85cbacce
gonzalezmaw <gonzalezmaw@yahoo.com>
2021-08-29 13:19:11 -0500

    Update - More information

--- QuickClassifiers.py
+++ QuickClassifiers.py
@@ -61,7 +61,7 @@ def QuickClassifiers():
         st.sidebar.write("Test size: ", parameter_test_size)
 
         st.sidebar.info("""
-                                [More information](http://gonzalezmaw.pythonanywhere.com/)
+                                [Short summary of the applicability of the method or model]
                                 """)
 
         uploaded_file = st.file_uploader("Choose a CSV file")
