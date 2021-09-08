705a5cc98bf1fa5807e1bbd09c8028bd9b2a2c58
gonzalezmaw <gonzalezmaw@yahoo.com>
2021-09-07 23:32:41 -0500

    Regression Description

--- RFR.py
+++ RFR.py
@@ -19,7 +19,7 @@ def RFR():  # Random Forest Regressor
         st.sidebar.write("n  estimators: ", parameter_n_estimators)
 
         st.sidebar.info("""
-            [Short summary of the applicability of the method or model]
+            Regression is a set of supervised machine learning methods that investigate the relationship between a dependent (target) variable and independent variable(s) (predictors). It is used to predict continuous, real-value quantities. (E.g.  predicting prices of a house given the features of the house like size and price).
             """)
 
         uploaded_file = st.file_uploader("Choose a CSV file")
